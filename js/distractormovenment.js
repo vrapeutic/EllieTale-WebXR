ffb35b39eee6764cb3ebf6671e83b7f777fa35ee
hadeergamal1 <hadeer.gamal@myvrapeutic.com>
2020-12-28 20:15:13 +0200

    update with comments

---
+++ js/distractormovenment.js
@@ -0,0 +1,46 @@
+AFRAME.registerComponent("moverandomly", {
+  
+    init: function()
+    {
+  
+      var target = document.querySelectorAll(".Box");//Array of targets
+      var ds = document.getElementById("myDistractor");// distractor element
+
+      console.log(target + " this " + ds+document.getElementById("level").getAttribute("value"));
+  
+     // level 2
+
+    console.log(document.getElementById("level").getAttribute("value"))
+    
+    let startDsMovement= function randommovenmet(index) 
+      {
+  
+        setTimeout(function() 
+        {
+    
+       randomIndexTarget=Math.floor(Math.random() * Math.floor(target.length));  
+    
+       nextTargetPosition=target[ randomIndexTarget].getAttribute("position");//  next target for distractor
+     
+      ds.setAttribute("animation","property:position; to:"+(nextTargetPosition.x+1.5)+" 0.5 "+nextTargetPosition.z+" dur:5000"); 
+    
+      
+    console.log(index+" here "+target[ randomIndexTarget].getAttribute("position").x+"ds "+ds.getAttribute("position").x)
+    
+      
+    
+          if (index >= target.length) 
+          {
+               
+               index = 0; // Set it back to `0` when it reaches `3`
+            }
+            console.log("i'm here 2")
+  
+            randommovenmet(index);
+      
+        }, 2000);
+    }
+      
+    startDsMovement( target.length);
+
+    }})
--- js/menu.js
+++ js/menu.js
@@ -1,50 +1,39 @@
-var url= "../images/particles/snowflake.png";
 
 AFRAME.registerComponent('menu', {
 init :function(){
 
-  var firtTargetPos=document.getElementById("targetone").getAttribute("position")
- var  lastTargetPos=document.getElementById("targetfour").getAttribute("position")
+  var firtTargetPos=document.getElementById("targetone").getAttribute("position");
+  var lastTargetPos=document.getElementById("targetfour").getAttribute("position");
  console.log(document.getElementById("selectenvironment").getAttribute("value"))
 
+//select level menu
 
-   /*   if(document.getElementById("selectenvironment").getAttribute("value")=="Beach" )
- {
-   url= "../images/particles/snowflake.png";
+ let  disableMenu=function Disable(){
 
-createBeachPartical();
-console.log(partical.id);
- }
- if(document.getElementById("selectenvironment").getAttribute("value")=="Garden" )
- {
-createBeachPartical();
- console.log(partical.id);
-
- }*/
-
-
- let  load=function fload(){
 document.getElementById("boxLevel").parentNode.removeChild( document.getElementById('boxLevel'))
 document.getElementById('boxLevel1').parentNode.removeChild(document.getElementById('boxLevel1'))
 document.getElementById('boxLevel2').parentNode.removeChild(document.getElementById('boxLevel2'))
 
-
 }
 let checkLevel=function fcheckLevel(){
   
-  document.getElementById("Taxi").setAttribute(
+
+  // set fairy position to target one
+  document.getElementById("Fairy").setAttribute(
  "animation",
  "property:position; to:"+(firtTargetPos.x + 1) +
  " 0.5 " +
  firtTargetPos.z +" dur:1000"
 )
-xpos=firtTargetPos.x + 1;
-zpos=firtTargetPos.z;
+fairyPositionx=firtTargetPos.x + 1;
+fairyPositionz=firtTargetPos.z;
+
+//level three (Set distractor component and partical )
 if(  document.getElementById("level").getAttribute("value")==3){ 
   
 //  document.getElementById("ds").parentNode.removeChild( document.getElementById("ds"));
  
-document.getElementById("myDs").setAttribute(
+document.getElementById("myDistractor").setAttribute(
  "animation",
  "property:position; to:"+(lastTargetPos.x+1) +
  " 0.5 " +
@@ -54,7 +43,7 @@ document.getElementById("myDs").setAttribute(
      var partical=document.createElement("a-entity")
 partical.setAttribute("spe-particles","texture:../images/particles/snowflake.png; color: #0000FF, #00FFFF, #FFFFFF; particle-count: 1000; acceleration: 0 -6 0;")
 partical.setAttribute("spe-particles","opacity: 2, 2, 0; velocity: 0 2 0; size: 2, 2, 0; velocity-spread: 2 0 2;") 
-partical.setAttribute("id", "DsPartical")
+partical.setAttribute("id", "distractingPartical")
  partical.setAttribute("position", 
         (lastTargetPos.x+1) +
         " 1 " +
@@ -63,54 +52,53 @@ partical.setAttribute("id", "DsPartical")
                  document.querySelector('a-scene').appendChild(partical);
 
        },1500); 
-// el.setAttribute("color", "#1dd4ed")
-document.getElementById("myDs").setAttribute('dsmove',"enabled",true)
-document.getElementById("myDs").setAttribute('visible',true);
-
+document.getElementById("myDistractor").setAttribute('advancedmovenment',"enabled",true)
+document.getElementById("myDistractor").setAttribute('visible',true);
 
  
-console.log(document.getElementById("myDs"));
+console.log(document.getElementById("myDistractor"));
 
 }
-else if(document.getElementById("level").getAttribute("value")==2){
-//    document.getElementById("dsmove").parentNode.removeChild(document.getElementById("dsmove"));
-document.getElementById("myDs").setAttribute('ds',"enabled",true)
-//   document.getElementById("myDs").removeAttribute('dsmove')
-var old_element = document.getElementById("myDs");
+//level two (Set distractor component and remove other components  )
+
+else if(document.getElementById("level").getAttribute("value")==2)
+{
+document.getElementById("myDistractor").setAttribute('moverandomly',"enabled",true)
+var old_element = document.getElementById("myDistractor");
 var new_element = old_element.cloneNode(true);
 old_element.parentNode.replaceChild(new_element, old_element);
-document.getElementById("myDs").setAttribute('visible',true);
+document.getElementById("myDistractor").setAttribute('visible',true);
 level=2
-console.log(document.getElementById("myDs")+ level);
+console.log(document.getElementById("myDistractor")+ level);
 
-// console.log(document.getElementById("myDs").childElementCount)
+// console.log(document.getElementById("myDistractor").childElementCount)
 
-document.getElementById("myDs").setAttribute(
+document.getElementById("myDistractor").setAttribute(
   "animation",
   "property:position; to:"+(lastTargetPos.x + 1) +
   " 0.5 " +
   lastTargetPos.z +" dur:1000"
 )
 }
-else{
-// document.getElementById("ds").parentNode.removeChild( document.getElementById("ds"));
-// document.getElementById("dsmove").parentNode.removeChild( document.getElementById("dsmove"));
-//console.log(document.getElementById("myDs").childElementCount);
-//   document.getElementById("myDs").removeAttribute('dsmove')
-// document.getElementById("myDs").removeAttribute('aabb-collider')
 
-var old_element = document.getElementById("myDs");
+//level one (set distractor without any movement)
+else
+{
+
+var old_element = document.getElementById("myDistractor");
 var new_element = old_element.cloneNode(true);
 old_element.parentNode.replaceChild(new_element, old_element);
-document.getElementById("myDs").setAttribute('visible',true);
-console.log(document.getElementById("myDs"));
+document.getElementById("myDistractor").setAttribute('visible',true);
+console.log(document.getElementById("myDistractor"));
 
 }
 }  
+//level menu buttons's events
+
 document.getElementById('boxLevel').addEventListener("click",e => 
 {
 checkLevel();
-load();
+disableMenu();
 console.log(document.getElementById("counter").getAttribute("value"))
 
 
@@ -118,7 +106,7 @@ console.log(document.getElementById("counter").getAttribute("value"))
 document.getElementById('boxLevel1').addEventListener("click",e => 
 {
 checkLevel();
-load();
+disableMenu();
 console.log(document.getElementById("level").getAttribute("value"))
 
 
@@ -126,20 +114,26 @@ console.log(document.getElementById("level").getAttribute("value"))
 document.getElementById('boxLevel2').addEventListener("click",e => 
 {
 checkLevel();
-load();
+disableMenu();
 console.log(document.getElementById("level").getAttribute("value"))
 
 });
-  //
+
+
+
+  //select timer menu
+
   if(document.querySelectorAll('.timer')!="undefined"){
+
   document.querySelectorAll('.timer').forEach(element => {
     element.addEventListener("click",e => {
-      document.getElementById("counter").setAttribute('closedtime',"enabled")
- Disable();
+      document.getElementById("counter").setAttribute('timecounter',"enabled")
+ selectTimer();
     })
   });
-let Disable= function fDisable(){
-        document.getElementById("boxtimer").parentNode.removeChild(document.getElementById('boxtimer'))
+let selectTimer= function time(){
+  
+document.getElementById("boxtimer").parentNode.removeChild(document.getElementById('boxtimer'))
 document.getElementById('boxtimer1').parentNode.removeChild(document.getElementById('boxtimer1'))
 document.getElementById('boxtimer2').parentNode.removeChild(document.getElementById('boxtimer2'))
 document.getElementById('noTimer').parentNode.removeChild(document.getElementById('noTimer'))
