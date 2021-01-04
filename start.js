b96494b0b8a7313da7a153b0d796ea855615c96e
yahyaalaamassoud <yahyaalaamassoud@gmail.com>
2021-01-03 20:42:19 -0500

    use port in environment variable

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-exec("set PORT=3000 && live-server --port=3000 --host=0.0.0.0 --no-browser", (error, data, getter) => {
+exec("live-server --port=$PORT --no-browser", (error, data, getter) => {
 	if(error){
 		console.log("error",error.message);
 		return;
